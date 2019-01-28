a824c9ab793f5a712293cdb2dc3bb6555f1b1425
RaulJP <rauljime12@hotmail.com>
2019-01-28 11:44:05 -0500

    Iniciando el proyecto

---
+++ src/main/java/pe/edu/unmsm/sisgiste/SisgisteApplication.java
@@ -0,0 +1,14 @@
+package pe.edu.unmsm.sisgiste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisgisteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisgisteApplication.class, args);
+	}
+
+}
+
